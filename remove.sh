615d63e7b933f64b46437b6bddf95eff2a15dbec
José Carlos García <jose@tux>
2018-06-17 00:38:54 +0200

    Added bloatware from OnePlus in OxygenOS.

--- remove.sh
+++ remove.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-APPS=("com.android.chrome" "com.google.android.googlequicksearchbox" "com.google.android.apps.tachyon" "com.google.android.apps.photos" "com.google.android.music" "com.google.android.videos")
+APPS=(
+    "com.android.chrome" "com.google.android.googlequicksearchbox" "com.google.android.apps.tachyon"
+    "com.google.android.apps.photos" "com.google.android.music" "com.oem.autotest" "net.oneplus.odm.provider"
+    "net.oneplus.odm" "com.oppo.bttestmode" "com.fingerprints.fingerprintsensortest" "com.google.android.videos"
+    "com.oneplus.factorymode.specialtest" "com.oneplus.factorymode"
+    ) 
 
 for APP in ${APPS[@]}; do
     echo "Uninstalling $APP"
